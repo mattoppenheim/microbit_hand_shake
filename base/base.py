8d5fbe947c8407f9755e9c69c877ae7f7d35b1c0
hardwaremonkey <matt.oppenheim@gmail.com>
2020-07-09 20:24:22 +0100

    Microbit can now be connected/disconnected without crashing the script

--- base/base.py
+++ base/base.py
@@ -2,37 +2,80 @@
  sends a trigger. This goes on the desktop as part of the hand_shake
  system. 
  https://www.seismicmatt.com/handshake/
+ May '20 - the microbit base station is now hot pluggable
  Matthew Oppenheim 2020 '''
 
 import keyboard
+import logging
 import serial
 import serial.tools.list_ports as list_ports
 import time
 
+BAUD = 115200
 PID_MICROBIT = 516
 VID_MICROBIT = 3368
 TIMEOUT = 0.1
 
 
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d %(message)s',
+    level=logging.INFO,
+    datefmt='%H:%M:%S')
+
+class Serial_Con():
+    ''' Create a serial connection in a context manager. '''
+
+    def __init__(self, comport, baud=BAUD):
+        self.comport = comport
+        self.baud = baud
+        logging.info('creating a serial connection')
+
+    
+    def __enter__(self):
+        ''' Return a serial port connection. '''
+        try:
+            self.serial_connection = serial.Serial(self.comport, self.baud,
+                rtscts=True, dsrdtr=True, timeout=TIMEOUT)
+            return self.serial_connection
+        except Exception as e:
+            logging.info('serial_connect error {}'.format(e))
+            return None
+
+
+    def __exit__(self, *args):
+        try:
+            self.serial_connection.close()
+        except Exception as e:
+            logging.info('failed to close serial_connection: {}'.format(e))
+        logging.info('Serial_Con closed')
+
+    
 def find_comport(pid, vid, baud):
     ''' Open the serial port with device with <pid> and <vid> connected. '''
     ser_port = serial.Serial(timeout = TIMEOUT)
     ser_port.baudrate = baud
     ports = list(list_ports.comports())
-    print('scanning ports')
+    logging.debug('scanning ports')
     for p in ports:
-        print('pid: {} vid: {}'.format(p.pid, p.vid))
+        logging.debug('pid: {} vid: {}'.format(p.pid, p.vid))
         if (p.pid == pid) and (p.vid == vid):
-            print('found target device pid: {} vid: {} port: {}'.format(p.pid, p.vid, p.device))
-            ser_port.port = str(p.device)
-            return ser_port
+            logging.debug('found target device pid: {} vid: {} port: {}'.format(p.pid, p.vid, p.device))
+            return str(p.device)
     return None
 
 
+def get_comport(pid, vid, baud):
+    ''' Get a serial connection to pid, vid. '''
+    comport = find_comport(pid, vid, baud)
+    while True:
+        if comport:
+            return comport
+        comport = find_comport(pid, vid, baud)
+        time.sleep(0.5)
+
+
 def detect_gesture(line):
-    print('{} microbit: {}'.format(time.strftime("%H:%M:%S"), line))
     if(line.strip() == "shake"):
-        print('shake detected')
         return True
     return False
 
@@ -48,23 +91,34 @@ def send_1_keystroke():
 
 
 def main():
-    print('looking for microbit')
-    ser_micro = find_comport(PID_MICROBIT, VID_MICROBIT, 115200)
-    if not ser_micro:
-        print('microbit not found')
-        return
-    print('opening and monitoring microbit port')
-    ser_micro.open()
+    logging.info('*** looking for a microbit')
     while True:
-        line = ser_micro.readline().decode('utf-8')
-        if line:  # If it isn't a blank line
-            if(detect_gesture(line)):
-              # F1 is the default key for control with Grid 3
-              send_f1_keystroke() 
-              # For testing
-              # send_1_keystroke() 
-    ser_micro.close()
-    ser_leo.close()
+        mbit_port = get_comport(PID_MICROBIT, VID_MICROBIT, 115200)
+        logging.info('microbit found at comport: {}'.format(mbit_port))
+        with Serial_Con(mbit_port) as mbit_serial:
+        # occassionally mbit_serial is not created, so is None
+            if not mbit_serial:
+                logging.info('failed to create mbit_serial')
+                time.sleep(0.5)
+                continue
+            logging.info('waiting to detect a shake')
+            while True:
+                try:
+                    line = mbit_serial.readline().decode('utf-8')
+                except serial.SerialException as e:
+                    logging.info('connection broken')
+                    logging.debug('exception: {}'.format(e))
+                    break
+                if line:  # If it isn't a blank line
+                    logging.debug('line: {}'.format(line))
+                if(detect_gesture(line)):
+                    # F1 is the default key for control with Grid 3
+                    send_f1_keystroke() 
+                    logging.info('*** shake detected')
+                    # For testing
+                    # python pythsend_1_keystroke() 
+                mbit_serial.flushInput()
+                time.sleep(0.1)
 
 
 if __name__ == '__main__':
