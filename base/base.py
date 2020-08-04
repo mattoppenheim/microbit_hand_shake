ecf0083f2b338a1e02da996e66a4d3880a5dc3f2
hardwaremonkey <matt.oppenheim@gmail.com>
2020-08-04 16:52:23 +0100

    Added admin and keystroke command line options

--- base/base.py
+++ base/base.py
@@ -2,29 +2,49 @@
  sends a trigger. This goes on the desktop as part of the hand_shake
  system. 
  https://www.seismicmatt.com/handshake/
- May '20 - the microbit base station is now hot pluggable
+ Windows only as the target software is Windows only.
+ May '20 - the microbit base station is now hot pluggable.
+ Aug'20 - command line options to run as admin or change keystroke. 
  Matthew Oppenheim 2020 '''
 
+import click
+from elevate import elevate
 import keyboard
 import logging
 import serial
 import serial.tools.list_ports as list_ports
+import sys
 import time
+try:
+    import win32gui
+except ModuleNotFoundError:
+    print('you need to install win32gui\npip install pywin32')
+    pass
 
+ADMIN_SOFTWARE = ['grid']
 BAUD = 115200
+# keystroke to broadcast when a shake is detected
+KEYSTROKE='F1'
 PID_MICROBIT = 516
+# software that requires this script to run in Administrator mode 
 VID_MICROBIT = 3368
 TIMEOUT = 0.1
 
+try:
+    # need the windll library to elevate to Administrator mode in windows
+    from ctypes import windll
+except ImportError:
+    print('need to run this on Windows if you want to use: {}'.format(ADMIN_SOFTWARE))
+    pass
 
 logging.basicConfig(
     format='%(asctime)s.%(msecs)03d %(message)s',
     level=logging.INFO,
     datefmt='%H:%M:%S')
 
+
 class Serial_Con():
     ''' Create a serial connection in a context manager. '''
-
     def __init__(self, comport, baud=BAUD):
         self.comport = comport
         self.baud = baud
@@ -80,17 +100,74 @@ def detect_gesture(line):
     return False
 
 
-def send_f1_keystroke():
-  ''' Send a software F1 keypress and release. '''
-  keyboard.press_and_release('F1')
+def target_admin_sware(software=ADMIN_SOFTWARE):
+    ''' Check if target software requires this script to run as Administrator. '''
+    toplist, winlist = [], []
+    logging.info('Looking for software that requires elevation to Aministrator: {}'
+        .format(ADMIN_SOFTWARE))
+    def _enum_cb(hwnd, results):
+        winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
+    win32gui.EnumWindows(_enum_cb, toplist)
+    for sware in software:
+        # winlist is a list of tuples (window_id, window title)
+        for hwnd, title in winlist:
+            if sware in title.lower():
+                logging.info('found software requiring Administrator mode {}'
+                    .format(title))
+                return True
+    logging.info('no software found that requires elevation to Administrator ')
+    return False
+
+
+def is_admin():
+    ''' Is the script running as an Administrator? '''
+    try:
+        return windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+        
+def send_keystroke(keystroke):
+  ''' Send a software keypress and release. '''
+  keyboard.press_and_release(keystroke)
 
 
 def send_1_keystroke():
-  ''' Send a software 1 keypress and release. '''
+  ''' Send a software keypress  of '1' and release. '''
   keyboard.press_and_release('1')
 
+@click.command()
 
+@click.option('-a', '--admin', default=False, 
+    help='Run as administrator. Required for Grid 3.')
+
+@click.option('-k', '--keystroke', default='F1',
+     help='Keystroke to send. Default is F1.')
+
+def main(admin, keystroke):
+    logging.info('software keystroke is {}'.format(keystroke))
+    logging.debug('admin flag is {}'.format(admin))
+    if is_admin():
+        logging.info('running as administrator')
+    else:
+        logging.info('not running as administrator')
+        if admin:
+            logging.info("restarting as administrator")
+            elevate()
+    service_microbit(keystroke)        
+
+'''
 def main():
+    if is_admin():
+        logging.info('running as admin')
+    else:
+        windll.shell32.ShellExecuteW(None, "runas", sys.executable,
+            " ".join(sys.argv), None, 0)
+        sys.exit("restarting as Administrator")
+    service_microbit()        
+'''
+
+def service_microbit(keystroke=KEYSTROKE):
     logging.info('*** looking for a microbit')
     while True:
         mbit_port = get_comport(PID_MICROBIT, VID_MICROBIT, 115200)
@@ -102,6 +179,7 @@ def main():
                 time.sleep(0.5)
                 continue
             logging.info('waiting to detect a shake')
+            shake_count = 1
             while True:
                 try:
                     line = mbit_serial.readline().decode('utf-8')
@@ -110,18 +188,21 @@ def main():
                     logging.debug('exception: {}'.format(e))
                     break
                 if line:  # If it isn't a blank line
-                    logging.debug('line: {}'.format(line))
+                    logging.debug('serial port: {}'.format(line))
                 if(detect_gesture(line)):
-                    # F1 is the default key for control with Grid 3
-                    send_f1_keystroke() 
-                    logging.info('*** shake detected')
+                    send_keystroke(keystroke) 
+                    logging.info('*** shake {} detected, {} sent'.format(
+                        shake_count, keystroke))
+                    shake_count += 1
                     # For testing
-                    # python pythsend_1_keystroke() 
+                    # send_1_keystroke() 
                 mbit_serial.flushInput()
                 time.sleep(0.1)
 
 
 if __name__ == '__main__':
+    # sys.argv line for testing
+    # sys.argv = ['', '--admin']
     main()
     print('exiting')
-
+    sys.exit()
