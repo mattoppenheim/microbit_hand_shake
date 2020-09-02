ec5e280fc459ff5f8f916e2d1fa3e149a234eb99
hardwaremonkey <matt.oppenheim@gmail.com>
2020-09-02 10:34:56 +0100

    Added correct receive.hex file to the repository.

--- receive/receive.py
+++ receive/receive.py
@@ -1,35 +1,40 @@
-''' receive shake and write to serial port
-update shake threshold to shake_detector '''
+""" receive shake and write to serial port
+update shake threshold to shake_detector """
 
 from microbit import *
 import radio
 
 radio.config(address=0x101000, group=40, channel=2, data_rate=radio.RATE_1MBIT)
-print('shake receive started')
+print("shake receive started")
 display.show(Image.DIAMOND)
 radio.on()
 
+
 def decrease_sensitivity():
-    ''' send message to decrease sensitivity '''
-    display.show('-')
-    radio.send('decrease')
+    """ send message to decrease sensitivity """
+    display.show("-")
+    radio.send("decrease")
     pause()
+
 
 def increase_sensitivity():
-    ''' send message to increase sensitivity '''
-    display.show('+')
-    radio.send('increase')
+    """ send message to increase sensitivity """
+    display.show("+")
+    radio.send("increase")
     pause()
 
+
 def pause():
-    ''' pause and clear display '''
+    """ pause and clear display """
     sleep(100)
     display.show(Image.DIAMOND)
 
+
 def shake_detected():
-    print('shake')
+    print("shake")
     display.show(Image.CHESSBOARD)
     pause()
+
 
 def main():
     while True:
@@ -39,7 +44,8 @@ def main():
             decrease_sensitivity()
         incoming = radio.receive()
         sleep(10)
-        if incoming == 'shake':
+        if incoming == "shake":
             shake_detected()
+
 
 main()
